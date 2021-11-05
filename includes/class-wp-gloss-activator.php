8f271b4dcefa487a8f9670678bf299cf24565cec
Joop Laan <joop.laan@gmail.com>
2021-11-05 06:58:16 +0100

    Scaffold

---
+++ includes/class-wp-gloss-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.jooplaan.com/
+ * @since      1.0.0
+ *
+ * @package    Wp_Gloss
+ * @subpackage Wp_Gloss/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Gloss
+ * @subpackage Wp_Gloss/includes
+ * @author     Joop Laan <wp-gloss@interconnecting.systems>
+ */
+class Wp_Gloss_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
