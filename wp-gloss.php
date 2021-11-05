912e5b85371b1a0e51f01bb78c396f0cc981d677
Joop Laan <joop.laan@gmail.com>
2021-11-05 10:12:27 +0100

    Update description

--- wp-gloss.php
+++ wp-gloss.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The plugin bootstrap file
  *
@@ -15,7 +14,7 @@
  * @wordpress-plugin
  * Plugin Name:       Glossarium
  * Plugin URI:        https://github.com/jooplaan/wp-gloss
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Build a glossary with preferred and non-preferred terms in a specialized field of knowledge.
  * Version:           1.0.0
  * Author:            Joop Laan
  * Author URI:        https://www.jooplaan.com/
