3c6422cfb695b966c76f7f1584cf46fef376f494
Joop Laan <joop.laan@gmail.com>
2021-11-07 08:03:07 +0100

    Add fields

--- includes/class-wp-gloss-meta-boxes.php
+++ includes/class-wp-gloss-meta-boxes.php
@@ -60,8 +60,11 @@ if ( ! class_exists( 'Wp_Gloss_Meta_Boxes' ) ) {
 			$meta = get_post_custom( $post->ID );
 			$wp_gloss_synonym = ! isset( $meta['wp-gloss-synonym'][0] ) ? '' : $meta['wp-gloss-synonym'][0];
 			$wp_gloss_sensitivity = ! isset( $meta['wp-gloss-sensitivity'][0] ) ? 0 : $meta['wp-gloss-sensitivity'][0];
+			$wp_gloss_term_preferred = ! isset( $meta['wp-gloss-term-preferred'][0] ) ? 0 : $meta['wp-gloss-term-preferred'][0];
+			$wp_gloss_term_non_preferred = ! isset( $meta['wp-gloss-term-non-preferred'][0] ) ? 0 : $meta['wp-gloss-term-non-preferred'][0];
 
-			wp_nonce_field( basename( __FILE__ ), 'glossary-term-meta-nonce' ); ?>
+			wp_nonce_field( basename( __FILE__ ), 'glossary-term-meta-nonce' );
+			?>
 
 			<table class="form-table form-table-wp-gloss-admin" role="presentation">
 				<tr>
@@ -74,6 +77,52 @@ if ( ! class_exists( 'Wp_Gloss_Meta_Boxes' ) ) {
 					</td>
 				</tr>
 
+				<tr>
+					<td class="team_meta_box_td" colspan="2">
+						<label for="wp-gloss-synonym"><?php esc_html_e( 'Preferred term', 'wp-gloss' ); ?></label>
+					</td>
+					<td colspan="4">
+						<?php
+							print( wp_kses(
+								$this->build_term_select( 'wp-gloss-term-preferred', $post->ID, $wp_gloss_term_preferred ),
+								array(
+									'select' => array(
+										'name' => array(),
+									),
+									'option' => array(
+										'name' => array(),
+										'selected' => array(),
+										'value' => array(),
+									),
+								)
+							)
+							);
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td class="team_meta_box_td" colspan="2">
+						<label for="wp-gloss-synonym"><?php esc_html_e( 'Non-Preferred term', 'wp-gloss' ); ?></label>
+					</td>
+					<td colspan="4">
+						<?php
+							print( wp_kses(
+								$this->build_term_select( 'wp-gloss-term-non-preferred', $post->ID, $wp_gloss_term_non_preferred ),
+								array(
+									'select' => array(
+										'name' => array(),
+									),
+									'option' => array(
+										'name' => array(),
+										'selected' => array(),
+										'value' => array(),
+									),
+								)
+							)
+							);
+						?>
+					</td>
+				</tr>
 				<tr>
 					<td class="team_meta_box_td" colspan="2">
 						<label for="wp-gloss-sensitivity"><?php esc_html_e( 'Sensitivity', 'wp-gloss' ); ?></label>
@@ -93,7 +142,7 @@ if ( ! class_exists( 'Wp_Gloss_Meta_Boxes' ) ) {
 									echo ' selected';
 								}
 								?>
-							><?php esc_html_e( 'Preferred term', 'wp-gloss' ); ?></option>
+							><?php esc_html_e( 'Sensitive term', 'wp-gloss' ); ?></option>
 							<option value="2"
 								<?php
 								if ( '2' == $wp_gloss_sensitivity ) {
@@ -119,7 +168,7 @@ if ( ! class_exists( 'Wp_Gloss_Meta_Boxes' ) ) {
 		/**
 		 * Save metaboxes.
 		 *
-		 * @param integet $post_id The post ID.
+		 * @param integer $post_id The post ID.
 		 *
 		 * @since 0.1.0
 		 */
@@ -146,15 +195,78 @@ if ( ! class_exists( 'Wp_Gloss_Meta_Boxes' ) ) {
 			}
 
 			// Check permissions.
-			if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+			if ( ! current_user_can( 'edit_post', $post_id ) ) {
 				return $post_id;
 			}
 
+			// Get the values from the $_POST object.
 			$meta['wp-gloss-synonym'] = ( isset( $_POST['wp-gloss-synonym'] ) ? sanitize_text_field( wp_unslash( $_POST['wp-gloss-synonym'] ) ) : '' );
 			$meta['wp-gloss-sensitivity'] = ( isset( $_POST['wp-gloss-sensitivity'] ) ? sanitize_text_field( wp_unslash( $_POST['wp-gloss-sensitivity'] ) ) : '' );
+			$meta['wp-gloss-term-preferred'] = ( isset( $_POST['wp-gloss-term-preferred'] ) ? sanitize_text_field( wp_unslash( $_POST['wp-gloss-term-preferred'] ) ) : '' );
+			$meta['wp-gloss-term-non-preferred'] = ( isset( $_POST['wp-gloss-term-non-preferred'] ) ? sanitize_text_field( wp_unslash( $_POST['wp-gloss-term-non-preferred'] ) ) : '' );
+
 			foreach ( $meta as $key => $value ) {
 				update_post_meta( $post_id, $key, $value );
 			}
+		}
+
+
+		/**
+		 * Build select form element for glossary terms..
+		 *
+		 * @param string  $name     The name of the SELECT element.
+		 * @param integer $post_id  The post ID.
+		 * @param integer $selected The selected.
+		 *
+		 * @since 0.1.0
+		 */
+		private function build_term_select( $name, $post_id, $selected = 0 ) {
+			$terms = $this->get_glossary_terms();
+
+			$html = '<select name="' . $name . '">';
+			$html .= '<option value="0"';
+			if ( 0 == $selected ) {
+				$html .= ' selected="selected"';
+			}
+			$html .= '>' . __( 'None', 'wp-gloss' ) . '</option>';
+
+			foreach ( $terms as $key => $value ) {
+				$html .= '<option value="' . $key . '"';
+				if ( $key == $selected ) {
+					$html .= ' selected="selected"';
+				}
+				$html .= '>' . $value['term'] . '</option>';
+			}
+			$html .= '</select>';
+			return $html;
+		}
+
+		/**
+		 * Get Glossary posts.
+		 *
+		 * @since 0.1.0
+		 */
+		private function get_glossary_terms() {
+			$terms = array();
+
+			// Set up query.
+			$args = array(
+				'post_type' => 'glossary-term',
+			);
+			$the_query = new WP_Query( $args );
+			// The Loop.
+			if ( $the_query->have_posts() ) {
+				while ( $the_query->have_posts() ) {
+					$the_query->the_post();
+					$id = get_the_ID();
+					$terms[ $id ]['term'] = get_the_title();
+				}
+			}
+
+			/* Restore original Post Data */
+			wp_reset_postdata();
+
+			return $terms;
 		}
 
 	}
