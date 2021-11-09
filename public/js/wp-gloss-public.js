8eb0c14dee04c02bc8370794f3b46a0dd6511a94
Joop Laan <joop.laan@gmail.com>
2021-11-09 16:19:07 +0100

    Add Show/hide on active/inactive link state + esc

--- public/js/wp-gloss-public.js
+++ public/js/wp-gloss-public.js
@@ -6,6 +6,7 @@
 	 */
 	$(function() {
 
+		// Show tooltip on hover.
 		$('.wp-gloss-tooltip-wrapper').hover(
 			function () {
 				$(this).find($('.wp-gloss-tooltip')).show();
@@ -14,6 +15,28 @@
 			function () {
 				$(this).find($('.wp-gloss-tooltip')).hide();
 				$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "true");
+			}
+		);
+
+		// Show tooltip on focus.
+		$('.wp-gloss-tooltip-wrapper').bind('focus', function() {
+			$(this).find($('.wp-gloss-tooltip')).show();
+			$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "false");
+		});
+		// Show tooltip on blur/no focus.
+		$('.wp-gloss-tooltip-wrapper').bind('blur', function() {
+			$(this).find($('.wp-gloss-tooltip')).hide();
+			$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "true");
+		});
+
+		// Hide the tooltip on Escape key press.
+		$(document).on(
+			'keydown',
+			function(event) {
+				if(event.key == "Escape") {
+					$('.wp-gloss-tooltip').hide();
+					$('.wp-gloss-tooltip').attr("aria-hidden", "true");
+				}
 			}
 		);
 	});
