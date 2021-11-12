162e2f6ffb366eac7d03f6726f4a8a8287c2932d
Joop Laan <joop.laan@gmail.com>
2021-11-12 15:58:04 +0100

    Show tooltip only if device is not mobile

--- public/js/wp-gloss-public.js
+++ public/js/wp-gloss-public.js
@@ -5,40 +5,46 @@
 	 * DOM ready
 	 */
 	$(function() {
+		var mobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+		// Show tooltip only if device is not mobile.
+		if ( mobile === false ) {
+			// Show tooltip on hover.
+			$('.wp-gloss-tooltip-wrapper').hover(
+				function () {
+					$(this).find($('.wp-gloss-tooltip')).show();
+					$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "false");
+				},
+				function () {
+					$(this).find($('.wp-gloss-tooltip')).hide();
+					$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "true");
+				}
+			);
 
-		// Show tooltip on hover.
-		$('.wp-gloss-tooltip-wrapper').hover(
-			function () {
+			// Show tooltip on focus.
+			$('.wp-gloss-tooltip-wrapper').bind('focus', function() {
 				$(this).find($('.wp-gloss-tooltip')).show();
 				$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "false");
-			},
-			function () {
+			});
+			// Show tooltip on blur/no focus.
+			$('.wp-gloss-tooltip-wrapper').bind('blur', function() {
 				$(this).find($('.wp-gloss-tooltip')).hide();
 				$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "true");
-			}
-		);
+			});
 
-		// Show tooltip on focus.
-		$('.wp-gloss-tooltip-wrapper').bind('focus', function() {
-			$(this).find($('.wp-gloss-tooltip')).show();
-			$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "false");
-		});
-		// Show tooltip on blur/no focus.
-		$('.wp-gloss-tooltip-wrapper').bind('blur', function() {
-			$(this).find($('.wp-gloss-tooltip')).hide();
-			$(this).find($('.wp-gloss-tooltip')).attr("aria-hidden", "true");
-		});
-
-		// Hide the tooltip on Escape key press.
-		$(document).on(
-			'keydown',
-			function(event) {
-				if(event.key == "Escape") {
-					$('.wp-gloss-tooltip').hide();
-					$('.wp-gloss-tooltip').attr("aria-hidden", "true");
+			// Hide the tooltip on Escape key press.
+			$(document).on(
+				'keydown',
+				function(event) {
+					if(event.key == "Escape") {
+						$('.wp-gloss-tooltip').hide();
+						$('.wp-gloss-tooltip').attr("aria-hidden", "true");
+					}
 				}
-			}
-		);
+			);
+
+		};
+
+
 	});
 
 })( jQuery );
